2a269965c57ae1b79087dd84f1099a1f2a832854
DARREN//YANG <32953546+darrenwhy@users.noreply.github.com>
2019-05-07 08:22:11 +1000

    base.py

---
+++ base.py
@@ -0,0 +1,20 @@
+print(" __  __ _______   _________       _____  ______ ")
+print("|  \/  |_   _\ \ / /__   __|/\   |  __ \|  ____|")
+print("| \  / | | |  \ V /   | |  /  \  | |__) | |__   ")
+print("| |\/| | | |   > <    | | / /\ \ |  ___/|  __|  ")
+print("| |  | |_| |_ / . \   | |/ ____ \| |    | |____ ")
+print("|_|  |_|_____/_/ \_\  |_/_/    \_\_|    |______|")
+print("//version 1.0.0 - developed by darrenwhy")
+
+yes = "powering on..."
+
+no = "powering off..."
+
+print(" ")
+print("launch? - options: yes // no")
+input("answer: ")
+
+if yes:
+    print(yes)
+    pass
+    print(no)
